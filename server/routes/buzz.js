8436a1828d5480e32a14468a7496accf76d97e0b
ravi@ttn.com <booler@ttn.com>
2019-06-19 09:40:31 +0530

    handled reactionPromises

--- server/routes/buzz.js
+++ server/routes/buzz.js
@@ -5,10 +5,6 @@ const router = Express.Router();
 const {multerUploads, dataUri} = require('../config/multer.config');
 const {uploader} = require('../config/cloudinary.config');
 
-// const multer = require('multer');
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage });
-
 router.post('/createBuzz', multerUploads, (req, res) => {
     try {
         // console.log(`request: `,req.headers);
@@ -26,7 +22,6 @@ router.post('/createBuzz', multerUploads, (req, res) => {
             });
             Promise.all(uploaderPromises).then((result) => {
                 req.body.images = result.map((file) => { return file.secure_url});
-                console.log(req.body.images)
                 createBuzzService(req.body).then(() => {
                     getNewBuzzs(req.body.startTime).then((buzzs) => {
                         var reactionPromises = [];
@@ -69,7 +64,7 @@ router.post('/getInitialBuzz', async (req, res) => {
     var buzzsWithReactions = await Promise.all(reactionPromises); //Promise.all([...reactionPromises]);
     let tempBuzzs = buzzs;
     buzzs = tempBuzzs.map((item, index) => {
-        item._doc['reactions'] = buzzsWithReactions[index]
+        item._doc['reactions'] = buzzsWithReactions[index];
         return item;
     });
     res.send({extractedBuzzs: buzzs, status: 1})
@@ -78,17 +73,19 @@ router.post('/getInitialBuzz', async (req, res) => {
 router.post('/getMoreBuzz', (req, res) => {
     const {limit, endTime} = req.body;
     var extractedBuzzs = [];
-    getMoreBuzzService(limit, endTime).then((buzzs) => {
+    getMoreBuzzService(limit, endTime).then(async (buzzs) => {
         extractedBuzzs = buzzs;
+        var reactionPromises = [];
         buzzs.forEach((buzz, index) => {
-            getReactionService(buzz._id).then((res1) => {
-                extractedBuzzs[index].reactions = res1;
-            }).catch((err) => console.error(err));
-            getCommentService(buzz._id).then((res) => {
-                extractedBuzzs[index].comments = res;
-            });
+            reactionPromises.push(getReactionService(buzz._id));
         });
-        res.send({extractedBuzzs, status: 1});
+        var buzzsWithReactions = await Promise.all(reactionPromises); //Promise.all([...reactionPromises]);
+        let tempBuzzs = buzzs;
+        buzzs = tempBuzzs.map((item, index) => {
+            item._doc['reactions'] = buzzsWithReactions[index];
+            return item;
+        });
+        res.send({extractedBuzzs: buzzs, status: 1});
     }).catch((err) => {
         console.error(err);
         res.send({message: 'DBError', status: 2});
