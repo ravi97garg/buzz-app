a4f8ab52ffebe06cef3cbd74fc203b55bbf9ce51
ravi@ttn.com <booler@ttn.com>
2019-06-21 21:04:56 +0530

    added modal

---
+++ app/src/components/modal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const modalRoot = document.getElementById('modal-root');
+
+class Modal extends React.Component {
+
+    componentDidMount() {
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+    }
+
+    render() {
+        return ReactDOM.createPortal(
+            <div className={'modal-overlay'}>
+                <div className={'modal-wrapper'}>
+                    {this.props.component}
+                </div>
+            </div>
+            , modalRoot)
+    }
+
+    componentWillUnmount() {
+        document.getElementsByTagName('body')[0].style.overflow = '';
+
+    }
+
+}
+
+export default Modal;
