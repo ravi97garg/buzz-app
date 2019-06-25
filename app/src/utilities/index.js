e4ac9fdd5fa77b6d8a7a8804af95a28b84171bf1
ravi@ttn.com <booler@ttn.com>
2019-06-25 10:45:25 +0530

    updated getTimeDifference

--- app/src/utilities/index.js
+++ app/src/utilities/index.js
@@ -5,8 +5,10 @@ const getToken = () => {
 const getTimeDifference = (time) => {
     let message = '';
     const timeDiff = Math.trunc((Date.now()-new Date(time))/(1000*60));
-    if(timeDiff < 60){
+    if(timeDiff < 1) {
         message = `just now`;
+    } else if(timeDiff >= 1 && timeDiff < 60){
+        message = `${Math.trunc(timeDiff%60)} minutes ago`;
     } else if(timeDiff >= 60 && timeDiff < 1440){
         message = `${Math.trunc(timeDiff/60)} hours ${Math.trunc(timeDiff%60)} minutes ago`;
     } else {
