22819a132a5944cd62ef9e471ec64b78c1b533b6
ravi@ttn.com <booler@ttn.com>
2019-06-25 10:48:42 +0530

    debugged error of res.send

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,10 +11,11 @@ const PRIVATE_KEY = require('../constants').JWT_KEY;
 router.post('/authenticate', (req, res) => {
     if (req.body.token) {
         jwt.verify(req.body.token, PRIVATE_KEY, function (err, decoded) {
-            res.send(decoded);
             if (err) {
                 console.error(err);
                 res.send({message: 'error while verifying in authenticate.js', status: 0})
+            } else {
+                res.send(decoded);
             }
         });
     } else {
