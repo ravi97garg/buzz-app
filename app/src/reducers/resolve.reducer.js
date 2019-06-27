441c7ace7720bcc0406f6bf05ebf1849d2a222a4
ravi@ttn.com <booler@ttn.com>
2019-06-27 17:11:36 +0530

    structured Resolve

--- app/src/reducers/resolve.reducer.js
+++ app/src/reducers/resolve.reducer.js
@@ -1,19 +1,56 @@
-const initialState = {complaintList: []};
+import {
+    GET_MY_RESOLVE_FAILED,
+    GET_MY_RESOLVE_STARTED,
+    GET_MY_RESOLVE_SUCCESS,
+    INIT_ALL_COMPLAINTS_FAILED,
+    INIT_ALL_COMPLAINTS_STARTED,
+    INIT_ALL_COMPLAINTS_SUCCESS, SET_RESOLVE_STATUS_DEFAULT,
+    UPDATE_COMPLAINT_STATUS_FAILED,
+    UPDATE_COMPLAINT_STATUS_STARTED,
+    UPDATE_COMPLAINT_STATUS_SUCCESS
+} from "../constants/resolve";
+import {STATUS} from "../constants";
 
-const complaint = (state = initialState, action) => {
+const initialState = {complaintList: [], myResolves: [], resolveStatus: STATUS.DEFAULT};
+
+const resolve = (state = initialState, action) => {
     switch (action.type) {
-        case 'INIT_ALL_COMPLAINTS':
-            return {...state, complaintList: action.payload.complaints};
+        case UPDATE_COMPLAINT_STATUS_STARTED:
+            return {...state, resolveStatus: UPDATE_COMPLAINT_STATUS_STARTED};
 
-        case 'UPDATE_COMPLAINT_STATUS':
+        case UPDATE_COMPLAINT_STATUS_FAILED:
+            return {...state, resolveStatus: UPDATE_COMPLAINT_STATUS_FAILED};
+
+        case UPDATE_COMPLAINT_STATUS_SUCCESS:
             const {complaintId, status} = action.payload;
             let complaintList = [...state.complaintList];
             complaintList = complaintList.map((item) => item._id === complaintId ? {...item, status: status} : item);
-            return {...state, complaintList}
+            return {...state, complaintList};
+
+        case GET_MY_RESOLVE_STARTED:
+            return {...state, resolveStatus: GET_MY_RESOLVE_STARTED};
+
+        case GET_MY_RESOLVE_SUCCESS:
+            return {...state, myResolves: [...action.data], resolveStatus: GET_MY_RESOLVE_SUCCESS};
+
+        case GET_MY_RESOLVE_FAILED:
+            return {...state, resolveStatus: GET_MY_RESOLVE_FAILED};
+
+        case INIT_ALL_COMPLAINTS_STARTED:
+            return {...state, resolveStatus: INIT_ALL_COMPLAINTS_STARTED};
+
+        case INIT_ALL_COMPLAINTS_SUCCESS:
+            return {...state, complaintList: action.payload.complaints, resolveStatus: INIT_ALL_COMPLAINTS_SUCCESS};
+
+        case INIT_ALL_COMPLAINTS_FAILED:
+            return {...state, resolveStatus: INIT_ALL_COMPLAINTS_FAILED};
+
+        case SET_RESOLVE_STATUS_DEFAULT:
+            return {...state, resolveStatus: STATUS.DEFAULT};
 
         default:
             return state;
     }
 };
 
-export default complaint;
+export default resolve;
