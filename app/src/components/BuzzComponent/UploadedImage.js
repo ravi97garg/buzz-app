1db3dce1a61bd680c00cdae2e13ecc70d61f8f4e
ravi@ttn.com <booler@ttn.com>
2019-07-05 23:07:06 +0530

    added ImageContainer

---
+++ app/src/components/BuzzComponent/UploadedImage.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {getRandomInt} from "../../utilities";
+import {faSearchPlus, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+class UploadedImageComponent extends Component {
+
+    createObjectURL = (file) => {
+        if (typeof file === "string") {
+            return file
+        } else if (window.webkitURL) {
+            return window.webkitURL.createObjectURL(file);
+        } else if (window.URL && window.URL.createObjectURL) {
+            return window.URL.createObjectURL(file);
+        } else {
+            return null
+        }
+    };
+
+    handleClick = () => {
+        this.props.onImageClick(this.props.UserController)
+    };
+
+    render() {
+        return (
+            <div
+                className={'upload-image-container'}
+                onClick={this.handleClick}
+            >
+                <img alt={getRandomInt(9999999)} src={this.createObjectURL(this.props.imageBlob)}/>
+                <div className={'image-overlay'}>
+                    {this.props.type === 'upload' ? <FontAwesomeIcon
+                        icon={faTrash}
+                        color="black"
+                        size="lg"
+                        title={'Delete Image'}
+                    /> : <FontAwesomeIcon
+                        icon={faSearchPlus}
+                        color="black"
+                        size="lg"
+                        title={'Zoom Image'}
+                    />}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UploadedImageComponent;
